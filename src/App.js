a3cedef9230f7d5161a5f3392a84852a682f3e98
Nicholas <zantladjevich@gmail.com>
2022-12-12 18:37:55 -0500

    Created new entry function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import DisaplyEntries from './Components/DisplayEntries/DisplayEntries';
+
 
 function App() {
 
   const[entries, setEntries] = useState([{Weight: 176, Date: '11-3-2021'}, {Weight: 140, Date: '11-3-2021'}])
 
+  function addNewEntry(entry){
+
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  };
+
   return (
     <div>
-      <DisaplyEntries parentEntries={entries}></DisaplyEntries>
+      <DisaplyEntries parentEntries={entries}/>
+      <AddEntryForm entryProperty={addNewEntry}/>
     </div>
   );
 }
